93fe1eefb78d5988b127df4002431f8ffeb006c0
AdhirathKumar <82048000+AdhirathKumar@users.noreply.github.com>
2024-03-19 18:15:54 +0530

    Update reverse.py

--- reverse.py
+++ reverse.py
@@ -1,12 +1,19 @@
-# Import necessary libraries
+import streamlit as st
 from newspaper import Article
 from googlesearch import search
-from angle_emb import AnglE
-from sklearn.metrics.pairwise import cosine_similarity
+from similarity import SIMILARITY_CHECK
 from trusted import check_iftrusted
 
 
-# Function to filter out social media URLs
+# Function to calculate age from published date
+def calculate_age(published_date, current_date):
+    age = current_date - published_date
+    years = age.days // 365
+    months = (age.days % 365) // 30
+    return years, months
+
+
+# Function to filter social media URLs
 def filter_social_media_urls(urls):
     filtered_urls = []
     for url in urls:
@@ -15,71 +22,57 @@ def filter_social_media_urls(urls):
     return filtered_urls
 
 
-# Function to perform a Google search based on the article URL
-def google_search(article_url):
-    search_query = f"related:{article_url}"
-    search_results = search(search_query, num_results=10)
-    return search_results
+def remove_extra_spaces(text):
+    words = text.split()
+    return " ".join(words)
 
 
-# Function to calculate Semantic Text Similarity (STS) score between two texts
-def calculate_sts_score(text1, text2):
-    angle = AnglE.from_pretrained(
-        "WhereIsAI/UAE-Large-V1", pooling_strategy="cls"
-    ).cuda()
-    vec1 = angle.encode(text1, to_numpy=True)
-    vec2 = angle.encode(text2, to_numpy=True)
-    similarity = cosine_similarity(vec1.reshape(1, -1), vec2.reshape(1, -1))[0][0]
-    return similarity
+# Set up Streamlit app
+st.title("Article Similarity Checker")
 
+# Input field for the user to enter the URL
+article_url = st.text_input("Enter the URL of the article")
 
-# Example URL of the news article
-# article_url = "https://indianexpress.com/article/opinion/columns/c-raja-mohan-donald-trump-christian-nationalism-opportunity-bjp-9183839/"
+# Button to trigger the search
+if st.button("Search"):
+    # Check if the URL is entered
+    if article_url:
+        # Download and parse the entered article
+        entered_article = Article(article_url)
+        entered_article.download()
+        entered_article.parse()
+        entered_article.nlp()
+        entered_article_text = entered_article.text
+        summary = entered_article.summary
+        current_article_title = entered_article.title
 
-# # Download and parse the entered article
-# entered_article = Article(article_url)
-# entered_article.download()
-# entered_article.parse()
-# entered_article_text = entered_article.text
-# title = entered_article.title
-
-# Check if the article is from a trusted source
-# if check_iftrusted(article_url):
-#     pass
-# else:
-#     # Perform a reverse search if the article has appeared before
-#     search_results = google_search(article_url)
-
-#     if search_results:
-#         print(
-#             "The article has appeared before. Retrieving information from matching articles:"
-#         )
-#         search_results = filter_social_media_urls(search_results)
-
-#         for result in search_results:
-#             # Initialize Article object with matching article URL
-#             matching_article = Article(result)
-#             matching_article.download()
-#             matching_article.parse()
-#             matching_article_text = matching_article.text
-#             current_title = matching_article.title
-
-#             # Calculate STS score between the entered article and matching article
-#             text_sts_score = calculate_sts_score(
-#                 entered_article_text, matching_article_text
-#             )
-
-#             print("Matching Article URL:", result)
-#             print("STS Score:", text_sts_score)
-
-#             title_sts_score = calculate_sts_score(title, current_title)
-
-#             if text_sts_score > 0.75 and title_sts_score > 0.75:
-#                 print("The article is similar to the entered article.")
-#             else:
-#                 print("The article is not similar to the entered article.")
-
-#             print("-----------------------------------")
-
-#     else:
-#         print("The article has not appeared before on the internet.")
+        # Perform a Google search using the article title
+        search_results = search(current_article_title)
+        search_results = filter_social_media_urls(search_results)
+        # Display the search results
+        for i, result in enumerate(search_results):
+            st.write(f"Result {i+1}: {result}")
+            domain, trusted = check_iftrusted(result)
+            st.write(f"Domain: {domain}, Trusted: {'Yes' if trusted else 'No'}")
+            searched_article = Article(result)
+            searched_article.download()
+            searched_article.parse()
+            searched_article.nlp()
+            searched_article_text = searched_article.text
+            cur_summary = searched_article.summary
+            searched_article_title = searched_article.title
+            class_text, confidence_text = SIMILARITY_CHECK(
+                entered_article_text, searched_article_text
+            )
+            class_title, confidence_title = SIMILARITY_CHECK(
+                current_article_title, searched_article_title
+            )
+            st.write("Text class and score:", class_text, confidence_text)
+            st.write("Title class and score:", class_title, confidence_title)
+            if (class_text == "True" and confidence_text > 0.6) or (
+                class_title == "True" and confidence_title > 0.6
+            ):
+                st.write("This is a similar article to the searched article.")
+            st.write("--------------------------------------------------------")
+    else:
+        st.warning("Please enter a valid URL.")
