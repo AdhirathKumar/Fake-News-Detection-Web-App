e25a3f557d53ed4b3fd5d2e36131bb6de9391582
AdhirathKumar <82048000+AdhirathKumar@users.noreply.github.com>
2024-03-19 18:15:19 +0530

    Update model_loader.py

--- model_loader.py
+++ model_loader.py
@@ -1,16 +1,18 @@
 # model_loader.py
 
 from transformers import pipeline
+from tensorflow.keras.models import load_model
+
+# from tensorflow.keras.models import load_model
 import google.generativeai as ga
 
 
 def load_models():
-    MODEL = "jy46604790/Fake-News-Bert-Detect"
-    TEXT_CLASSIFIER = pipeline("text-classification", model=MODEL, tokenizer=MODEL)
-
+    # MODEL = "jy46604790/Fake-News-Bert-Detect"
+    # TEXT_CLASSIFIER = pipeline("text-classification", model=MODEL, tokenizer=MODEL)
+    TEXT_CLASSIFIER = load_model(r"D:\Final Project\pages\fake_news_bert.h5")
     GOOGLE_API_KEY = "AIzaSyDecPEPHA2dNPF3E2W0a77u5Yvr_7YQtrg"
     ga.configure(api_key=GOOGLE_API_KEY)
-
-    SUMMARIZATION_MODEL = ga.GenerativeModel(model_name="gemini-pro")
     CAPTIONING_MODEL = ga.GenerativeModel(model_name="gemini-pro-vision")
-    return TEXT_CLASSIFIER, SUMMARIZATION_MODEL, CAPTIONING_MODEL
+
+    return TEXT_CLASSIFIER, CAPTIONING_MODEL
